e0880c563ca57088d367d1f2711be5a29d2d55da
Francois Genolini <not@my.email>
2023-04-07 22:45:18 +0100

    refactor

--- src/main_loop.c
+++ src/main_loop.c
@@ -3,36 +3,39 @@
 #include "init.h"
 #include "load_resources.h"
 
-bool has_sill_texture = false;
+#define dice_radius 50.0f
+#define dice_width dice_radius * 2.0f
+
 int sill_texture_x = 0;
 int sill_texture_y = 0;
+bool has_sill_texture = false;
 
-static float delta_time = 0.0f;
+static const Vector2 dice_origin = {dice_radius, dice_radius};
+
 static Vector2 dice_position = {0};
 static Vector2 dice_speed = {200.0f, 140.0f};
-static float dice_radius = 50.0f;
-static Vector2 dice_size = {0};
+static float delta_time = 0.0f;
 static float dice_rotation = 0.0f;
+static float height_minus_dice = 0.0f;
 static float rotation_speed = 20.0f;
-static Vector2 dice_origin = {0};
+static float width_minus_dice = 0.0f;
 
 static void initial_variables()
 {
     dice_position.x = screen_width / 2.0f;
     dice_position.y = screen_height / 2.0f;
-    dice_size.x = dice_radius * 2.0f;
-    dice_size.y = dice_radius * 2.0f;
-    dice_origin = (Vector2){dice_radius, dice_radius};
+    width_minus_dice = screen_width - dice_radius;
+    height_minus_dice = screen_height - dice_radius;
 }
 
-static bool out_of_bounds_x(float x, float width)
+static bool out_of_bounds_x(float x)
 {
-    return x + width >= screen_width || x <= width;
+    return x <= dice_radius || x >= width_minus_dice;
 }
 
-static bool out_of_bounds_y(float y, float height)
+static bool out_of_bounds_y(float y)
 {
-    return y + height >= screen_height || y <= height;
+    return y <= dice_radius || y >= height_minus_dice;
 }
 
 static void update_variables()
@@ -41,18 +44,16 @@ static void update_variables()
     dice_position.x += dice_speed.x * delta_time;
     dice_position.y += dice_speed.y * delta_time;
     dice_rotation += rotation_speed * delta_time;
-    float speed = rotation_speed;
-    if (out_of_bounds_x(dice_position.x, dice_radius))
+    if (out_of_bounds_x(dice_position.x))
     {
         dice_speed.x *= -1.0f;
         rotation_speed *= -1.0f;
     }
 
-    if (out_of_bounds_y(dice_position.y, dice_radius))
+    if (out_of_bounds_y(dice_position.y))
     {
         dice_speed.y *= -1.0f;
-        if (speed == rotation_speed)
-            rotation_speed *= -1.0f;
+        rotation_speed *= -1.0f;
     }
 }
 
@@ -64,7 +65,7 @@ static void draw_contents()
     DrawText("press escape to exit",
              190, 200, 20, LIGHTGRAY);
     DrawRectanglePro(
-        (Rectangle){dice_position.x, dice_position.y, dice_size.x, dice_size.y},
+        (Rectangle){dice_position.x, dice_position.y, dice_width, dice_width},
         dice_origin, dice_rotation, DARKGREEN);
 }
 
